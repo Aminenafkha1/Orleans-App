39d0d7b25f315025f6f145036e7909cd4406f00a
aminenafkha1 <mohamed.amine.nafkha@horizon-tech.tn>
2024-05-17 17:17:10 +0100

    testd

--- Softylines.OrleansApp.Silo/Program.cs
+++ Softylines.OrleansApp.Silo/Program.cs
@@ -1,57 +1,60 @@
-﻿ 
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Orleans.Configuration;
-using System.Net;
-using Softylines.OrleansApp.Silo;
 using System.Net.Sockets;
+using System.Net;
 
 try
 {
-    await StartSilo(args);
+    using IHost host = await StartSiloAsync();
+    Console.WriteLine("\n\n Press Enter to terminate...\n\n");
+    Console.ReadLine();
+
+    await host.StopAsync();
+
     return 0;
 }
 catch (Exception ex)
 {
     Console.WriteLine(ex);
     return 1;
-} 
-static async Task<IHost> StartSilo(string[ ] args)
+}
+static async Task<IHost> StartSiloAsync()
 {
-  
-
-    string sqlServerConnectionString = "Server=192.168.30.35;Database=OrleansDb;User Id=sa;Password=Anis2004#;";
-     var name = Dns.GetHostName(); // get container id
+    var persistanceConnectionString = "Server=192.168.30.55;Database=orleans;User Id=sa;Password=Monemnejijannet1;";
+    var name = Dns.GetHostName(); // get container id
     var ip = Dns.GetHostEntry(name).AddressList.FirstOrDefault(x => x.AddressFamily == AddressFamily.InterNetwork);
-
-    int siloPort = 11111;
-    int gatewayPort = 30000;
-    var builder = Host.CreateDefaultBuilder()
+    var builder = Host
+        .CreateDefaultBuilder()
         .UseOrleans((context, silo) =>
         {
             silo
-                .UseAdoNetClustering(options =>
-                {
-                    options.Invariant = "System.Data.SqlClient";
-                    options.ConnectionString = sqlServerConnectionString;
-                })
                 .Configure<ClusterOptions>(options =>
                 {
                     options.ClusterId = "aminedev";
-                    options.ServiceId = "aniss";
-                }) 
-                .ConfigureEndpoints(siloPort: siloPort, gatewayPort: gatewayPort)
+                    options.ServiceId = "amineservice";
+                })
+                .UseAdoNetClustering(options =>
+                {
+                    options.ConnectionString = persistanceConnectionString ?? " ";
 
-                // .ConfigureEndpoints(default, default,  default, listenOnAnyHostAddress: true)
-                .AddAdoNetGrainStorage(
-                        name: "GrainStore",
-                        configureOptions: options =>
-                        {
-                            options.ConnectionString = sqlServerConnectionString;
-                        }) 
-                .UseDashboard(options =>
+                    options.Invariant = "System.Data.SqlClient";
+                }).AddAdoNetGrainStorage("profiles", options =>
+                {
+                    options.ConnectionString = persistanceConnectionString ?? " ";
+                    options.Invariant = "System.Data.SqlClient";
+                }).AddAdoNetGrainStorage("profile", options =>
+                {
+                    options.ConnectionString = persistanceConnectionString ?? " ";
+                    options.Invariant = "System.Data.SqlClient";
+                }).Configure<EndpointOptions>(options =>
+                {
+                    options.AdvertisedIPAddress = ip;
+                    options.SiloPort = 11111; 
+                    options.GatewayPort = 30000;
+                }).ConfigureLogging(logging => logging.AddConsole()).UseDashboard(options =>
                 {
                     options.Username = "username";
                     options.Password = "password";
@@ -59,15 +62,12 @@ static async Task<IHost> StartSilo(string[ ] args)
                     options.Port = 9000;
                     options.HostSelf = true;
                     options.CounterUpdateIntervalMs = 1000;
-                }) 
-                 .ConfigureLogging(logging =>
-                {
-                    logging
-                        .AddConsole();
-                }); 
+            }); ;
+
         });
 
-    var app = builder.Build();
-    await app.RunAsync();
-}
+    var host = builder.Build();
+    await host.RunAsync();
 
+    return host;
+}
